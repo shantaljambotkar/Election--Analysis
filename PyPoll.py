ba8574f69acd63986fa8a4337d093260281a3a05
shantaljambotkar <88519257+shantaljambotkar@users.noreply.github.com>
2021-08-30 14:38:59 -0400

    first commit

---
+++ PyPoll.py
@@ -0,0 +1,65 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+
+# import the datetime class from the datetime module.
+import datetime
+#Use the now() attribute on the datetime class to get the present time
+now = datetime.datetime.now()
+#Print the present time
+print("the time right now is ", now)
+
+#Add our dependencies
+import csv
+import os
+
+# Assign a variable for the file to load and the path to the file.
+file_to_load = os.path.join("Resources","election_results.csv") #for different paths -- os.path.join("foldername1","Foldername2","election_results.csv") 
+
+#Create a filename variable to a direct or indirect path to the file
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+#with open(file_to_load) as election_data:
+
+    # To do: Read and analysis.
+    #print(election_data)
+    #Read the file object with the reader function
+    #file_reader = csv.reader(election_data)
+
+    #Print each row in csv file
+    # for row in file_reader:
+    #     print(row)
+
+#Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    #Read and print the header row
+    headers = next(file_reader)
+    print(headers)
+
+
+    
+
+# Close the file.
+election_data.close()
+
+
+
+# #Using the open() function with the "w" mode we will write data to the file
+# open(file_to_save, "w")
+
+#Using with statement open the file as a text file
+with open(file_to_save, "w") as txt_file:
+
+    #Write some data
+    #outfile.write("Hello World")
+    #txt_file.write("Hello world")
+    txt_file.write("Counties in the Election\n_______________________________\nArapahoe\nDenver\nJefferson ")
+
+#close the file
+txt_file.close()
--- Python_practice.py
+++ Python_practice.py
@@ -1,8 +1,52 @@
-# counties = ['A','B','c','D','E','F']
-# print(counties)
-# counties.append('G')
-# print(counties)
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
 
-counties_tuple=['Arapahoe','Denver','Jefferson']
-print(counties_tuple) 
-print(counties_tuple[:-1])
+
+# dictionary1 = {
+#     1: "AAAA",
+#     2: "DDDDD",
+#     3: "RRRRR"
+# }
+
+# for key, value in dictionary1.items():
+#      print(f"value has {key} voters.")                           #print(value + " count has " + str(key) + " registered votes")
+    
+""" print(dictionary1)
+
+print(dictionary1.keys())
+
+print(dictionary1.values())
+
+dictionary1.items()
+print(key,value) """ 
+
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes} number of votes. "
+#     f"The total number of votes in the election was {total_votes}. "
+#     f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+# print(message_to_candidate)
+
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# for key, value  in counties_dict.items():
+#     print(f"{key}  county has {value} reg vot")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+               {"county":"Denver", "registered_voters": 463353}, 
+               {"county":"Jefferson", "registered_voters": 432438}]
+
+#for county, registered_voters in voting_data:
+for i in range(len(voting_data)):
+ print(f"{voting_data[i]['county']} and reg voter are {voting_data[i]['registered_voters']}")
+    
+#{voting_data.registered_voters}")
+#county has registered_voters reg vot"
+
+# for county_dict in voting_data:
+#     for county,registered_voters in county_dict:
+#         print(f" {county} {registered_voters}")
+
+
